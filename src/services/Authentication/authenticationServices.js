1fab205ab786034e14056b6db4403d793300bbcf
unknown <rahulkumar8111056369@gmail.com>
2024-09-24 17:13:34 +0530

    google authentication

--- src/services/Authentication/authenticationServices.js
+++ src/services/Authentication/authenticationServices.js
@@ -4,30 +4,37 @@ const { GENERATE_TOKEN } = require("../../confic/JWT");
 
 const authentiCationService = async (input, output) => {
     const { emailId, hashedPassword: password, mobilenumber, code: otp } = input;
-    console.log(input);
-
     const authenticationQuery = `
-        INSERT INTO users (email, password, phone_number) VALUES (?, ?, ?);
+        INSERT INTO users_credentials (email, password_hash, phone_number) VALUES (?, ?, ?);
         SET @last_user_id = LAST_INSERT_ID();
         INSERT INTO otps (user_id, otp_code, expires_at) 
         VALUES (@last_user_id, ?, DATE_ADD(NOW(), INTERVAL 5 MINUTE));
-        SELECT * FROM users WHERE user_id = @last_user_id;
+        SELECT * FROM users_credentials WHERE user_id = @last_user_id;
     `;
     db.query(authenticationQuery, [emailId, password, mobilenumber, otp], async (err, result) => {
         if (err) {
             output({ error: { description: err.message } }, null);
             console.log(err);
         } else {
+
             const saltRounds = 10;
             const user = result[3][0].user_id;
-            const token = await bcrypt.hash(otp.toString(), saltRounds);
-            const data = { user, token };
+            const verifycode = await bcrypt.hash(otp.toString(), saltRounds);
+            const token = await GENERATE_TOKEN(otp, '1h');
+            const data = { user, verifycode, token };
+            const insertToken = `INSERT INTO tokens (user_id, token, expires_at) VALUES (?, ?, DATE_ADD(NOW(), INTERVAL 5 MINUTE));`; db.query(insertToken, [user.user_id, token], (err, results) => {
+                if (err) {
+                    console.error("Database query error:", err);
+                    return output({ error: { description: err.message } }, null);
+                }
+                next();
+            })
+            console.log(data);
+
             output(null, data);
-            console.log(token);
         }
     });
 };
-
 
 const otpVerificationServieces = async (input, output) => {
     const verifiCationOtp = input.verificationotp;
@@ -83,5 +90,84 @@ const processLoginServieces = async (input, output) => {
     });
 
 }
+const geustSignServices = (input, output) => {
+    const { email, mobilenumber, code: otp } = input;
+    const checkUserQuery = `SELECT * FROM users WHERE email = ? OR phone_number = ?`;
+    db.query(checkUserQuery, [email, mobilenumber], async (err, result) => {
+        if (err) {
+            output({ error: { description: err.message } }, null);
+            return;
+        }
+        if (result.length > 0) {
+            console.log(result);
 
-module.exports = { authentiCationService, otpVerificationServieces, processLoginServieces };
+            const saltRounds = 10;
+            const userId = result[0].user_id;
+            const token = await bcrypt.hash(otp.toString(), saltRounds);
+            const data = { success: true, userId, description: "User Already Exsist" };
+            output(null, { ...data });
+        }
+        else {
+            const geustSignQuery = `
+                        INSERT INTO users (email, phone_number) VALUES (?, ?);
+                        SET @last_user_id = LAST_INSERT_ID();
+                        SELECT * FROM users WHERE user_id = @last_user_id;`;
+            db.query(geustSignQuery, [email, mobilenumber], async (err, result) => {
+                if (err) {
+                    output({ error: { description: err.message } }, null);
+                    console.log(err);
+                } else {
+                    const saltRounds = 10;
+                    const userId = result[2][0].user_id;
+                    const token = await bcrypt.hash(otp.toString(), saltRounds);
+                    const data = { success: true, userId, description: "User created successfully" };
+                    output(null, data);
+                }
+            });
+        }
+
+    });
+}
+
+const googleAuthentiCationServices = async (input, output) => {
+    const { Email } = input;
+    const checkUserQuery = `SELECT * FROM users WHERE email = ?`;
+    db.query(checkUserQuery, [Email], async (err, result) => {
+
+        if (err) {
+            output({ error: { description: err.message } }, null);
+        }
+        if (result.length > 0) {
+            console.log(result);
+            // const saltRounds = 10;
+            const userId = result[0].user_id;
+            // const token = await bcrypt.hash(otp.toString(), saltRounds);
+            const data = { success: true, userId, description: "User Already Exsist" };
+            output(null, { ...data });
+        }
+        else {
+            const geustSignQuery = `
+                        INSERT INTO users (email) VALUES (?);
+                        SET @last_user_id = LAST_INSERT_ID();
+                        SELECT * FROM users WHERE user_id = @last_user_id;`;
+            db.query(geustSignQuery, [Email], async (err, result) => {
+                console.log(Email);
+
+                if (err) {
+                    output({ error: { description: err.message } }, null);
+                    console.log(err);
+                } else {
+                    // const saltRounds = 10;
+                    const userId = result[2][0].user_id;
+                    console.log(result);
+                    
+                    // const token = await bcrypt.hash(otp.toString(), saltRounds);
+                    const data = { success: true, userId, description: "User created successfully" };
+                    output(null,data);
+                }
+            });
+        }
+
+    });
+}
+module.exports = { authentiCationService, otpVerificationServieces, googleAuthentiCationServices, geustSignServices, processLoginServieces };
