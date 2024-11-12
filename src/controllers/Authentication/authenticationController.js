f11bdad2d1e9c524f1af0cb11c19cbab2564a674
RaviKGR <ravimuthukgr@gmail.com>
2024-11-12 15:24:05 +0530

    user

--- src/controllers/Authentication/authenticationController.js
+++ src/controllers/Authentication/authenticationController.js
@@ -1,143 +1,170 @@
 const sendOtpMail = require("../../email/emailconfic");
-const bcrypt = require('bcrypt');
-const { promisify } = require('util');
-const { authentiCationService, otpVerificationServieces, processLoginServieces, geustSignServices, googleAuthentiCationServices } = require("../../services/Authentication/authenticationServices");
+const bcrypt = require("bcrypt");
+const { promisify } = require("util");
+const {
+  authentiCationService,
+  otpVerificationServieces,
+  processLoginServieces,
+  geustSignServices,
+  googleAuthentiCationServices,
+  createUserService,
+} = require("../../services/Authentication/authenticationServices");
 
 const addAuthenticationController = async (req, res) => {
-    try {
-        const { emailId, password, mobilenumber } = req.body;
-        if (!emailId && !mobilenumber) {
-            return res.status(400).json({ error: "Email address or mobile number is required" });
-        }
-        if (!password) {
-            return res.status(400).json({ error: "Password is required" });
-        }
-        const saltRounds = 10;
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-        const code = Math.floor(1000 + Math.random() * 9000);
-        const input = { emailId, hashedPassword, code, mobilenumber };
-        const data = await new Promise((resolve, reject) => {
-            authentiCationService(input, (err, result) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(result);
-                }
-            });
-        });
-        // Send the OTP email
-        const mailSuccess = await sendOtpMail(emailId, data);
-        if (mailSuccess) {
-            return res.status(200).json({ success: true, message: 'User registered successfully. OTP sent to email.' });
-        }
-        else {
-            return res.status(500).json({ error: 'Failed to send OTP email. Please try again.' });
-        }
-    } catch (error) {
-        console.error("Error in addAuthenticationController:", error);
-        return res.status(500).json({ error: "Internal server error" });
+  try {
+    const { emailId, password, mobilenumber } = req.body;
+    if (!emailId && !mobilenumber) {
+      return res
+        .status(400)
+        .json({ error: "Email address or mobile number is required" });
     }
+    if (!password) {
+      return res.status(400).json({ error: "Password is required" });
+    }
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const code = Math.floor(1000 + Math.random() * 9000);
+    const input = { emailId, hashedPassword, code, mobilenumber };
+    const data = await new Promise((resolve, reject) => {
+      authentiCationService(input, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+    // Send the OTP email
+    const mailSuccess = await sendOtpMail(emailId, data);
+    if (mailSuccess) {
+      return res
+        .status(200)
+        .json({
+          success: true,
+          message: "User registered successfully. OTP sent to email.",
+        });
+    } else {
+      return res
+        .status(500)
+        .json({ error: "Failed to send OTP email. Please try again." });
+    }
+  } catch (error) {
+    console.error("Error in addAuthenticationController:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
 };
-
 
 const otpVerifiCationController = async (req, res) => {
-    try {
-        const { verificationotp } = req.body;
-        const userIdverificationotp = req.query.userId;
-        const input = { verificationotp, userIdverificationotp };
-        if (!verificationotp || !userIdverificationotp) {
-            return res.status(400).send("Check the otp");
-        }
-        else {
-            await otpVerificationServieces(input, (err, data) => {
-                if (err) res.status(400).send(err.error);
-                else res.status(200).send(data);
-                console.log(data);
-            })
-        }
-    } catch (error) {
-        res.status(500).send({ error: "Internal server error" })
+  try {
+    const { verificationotp } = req.body;
+    const userIdverificationotp = req.query.userId;
+    const input = { verificationotp, userIdverificationotp };
+    if (!verificationotp || !userIdverificationotp) {
+      return res.status(400).send("Check the otp");
+    } else {
+      await otpVerificationServieces(input, (err, data) => {
+        if (err) res.status(400).send(err.error);
+        else res.status(200).send(data);
+        console.log(data);
+      });
     }
-}
-
-const userLoginController = async (req, res) => {
-    try {
-        const UserMailId = req.body.email;
-        const UserPassword = req.body.password;
-        const input = { UserMailId, UserPassword }
-        if (!UserMailId || !UserPassword) {
-            res.status(400).send("Check the data")
-        }
-        else {
-            await processLoginServieces(input, (err, data) => {
-                if (err) res.status(400).send(err.error);
-                else res.status(200).send(data);
-            })
-        }
-    } catch (error) {
-        return res.status(500).json({ error: "Internal server error" })
-    }
-}
-const geustSignController = async (req, res) => {
-    const { email, mobilenumber } = req.body;
-    const code = Math.floor(1000 + Math.random() * 9000);
-    const input = { email, code, mobilenumber };
-    try {
-        if (!email && !mobilenumber) {
-            res.status(400).send("Check the data");
-        }
-        const guestSignServiceAsync = promisify(geustSignServices);
-        const data = await guestSignServiceAsync(input);
-        if (!data.success) {
-            return res.status(200).json({ ...data });
-        }
-        return res.status(200).json({ ...data });
-    } catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: 'Internal server error',
-            error: error.message,
-        })
-    }
-}
-
-const googleAuthenticationController = async (req, res) => {
-    const Email = req.body;
-    try {
-        if (!Email) {
-            return res.status(400).json({
-                success: false,
-                message: 'check The Field'
-            })
-        }
-        else {
-            await googleAuthentiCationServices(req.body, (err, data) => {
-                if (err) {
-                    return res.status(400).send(err.error)
-                }
-                else {
-                    return res.status(200).json({
-                        success: true,
-                        ...data,
-                    })
-                }
-            })
-        }
-
-    } catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: 'Internal server error',
-            error: error.message,
-        })
-    }
-}
-
-const signOutController = async (req, res) => {
-    req.session.destroy((err) => {
-        if (err) return res.status(500).json({ message: "Failed to sign out" });
-        res.status(200).json({ message: "SignOut successful" });
-    });
+  } catch (error) {
+    res.status(500).send({ error: "Internal server error" });
+  }
 };
 
-module.exports = { addAuthenticationController, googleAuthenticationController, otpVerifiCationController, userLoginController, geustSignController, signOutController };
+const userLoginController = async (req, res) => {
+  try {
+    const UserMailId = req.body.email;
+    const UserPassword = req.body.password;
+    const input = { UserMailId, UserPassword };
+    if (!UserMailId || !UserPassword) {
+      res.status(400).send("Check the data");
+    } else {
+      await processLoginServieces(input, (err, data) => {
+        if (err) res.status(400).send(err.error);
+        else res.status(200).send(data);
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+const geustSignController = async (req, res) => {
+  const { email, mobilenumber } = req.body;
+  const code = Math.floor(1000 + Math.random() * 9000);
+  const input = { email, code, mobilenumber };
+  try {
+    if (!email && !mobilenumber) {
+      res.status(400).send("Check the data");
+    }
+    const guestSignServiceAsync = promisify(geustSignServices);
+    const data = await guestSignServiceAsync(input);
+    if (!data.success) {
+      return res.status(200).json({ ...data });
+    }
+    return res.status(200).json({ ...data });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
+
+const googleAuthenticationController = async (req, res) => {
+  const Email = req.body;
+  try {
+    if (!Email) {
+      return res.status(400).json({
+        success: false,
+        message: "check The Field",
+      });
+    } else {
+      await googleAuthentiCationServices(req.body, (err, data) => {
+        if (err) {
+          return res.status(400).send(err.error);
+        } else {
+          return res.status(200).json({
+            success: true,
+            ...data,
+          });
+        }
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
+
+const signOutController = async (req, res) => {
+  req.session.destroy((err) => {
+    if (err) return res.status(500).json({ message: "Failed to sign out" });
+    res.status(200).json({ message: "SignOut successful" });
+  });
+};
+
+const createUserController = async (req, res) => {
+    try {
+        const result = await createUserService();
+        return res.status(201).json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json( {message: "Internal server error"} )
+    }
+};
+
+module.exports = {
+  addAuthenticationController,
+  googleAuthenticationController,
+  otpVerifiCationController,
+  userLoginController,
+  geustSignController,
+  signOutController,
+  createUserController,
+};
--- src/services/Authentication/authenticationServices.js
+++ src/services/Authentication/authenticationServices.js
@@ -1,173 +1,214 @@
 const { db } = require("../../confic/db");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const { GENERATE_TOKEN } = require("../../confic/JWT");
 
 const authentiCationService = async (input, output) => {
-    const { emailId, hashedPassword: password, mobilenumber, code: otp } = input;
-    const authenticationQuery = `
+  const { emailId, hashedPassword: password, mobilenumber, code: otp } = input;
+  const authenticationQuery = `
         INSERT INTO users_credentials (email, password_hash, phone_number) VALUES (?, ?, ?);
         SET @last_user_id = LAST_INSERT_ID();
         INSERT INTO otps (user_id, otp_code, expires_at) 
         VALUES (@last_user_id, ?, DATE_ADD(NOW(), INTERVAL 5 MINUTE));
         SELECT * FROM users_credentials WHERE user_id = @last_user_id;
     `;
-    db.query(authenticationQuery, [emailId, password, mobilenumber, otp], async (err, result) => {
-        if (err) {
-            output({ error: { description: err.message } }, null);
-            console.log(err);
-        } else {
+  db.query(
+    authenticationQuery,
+    [emailId, password, mobilenumber, otp],
+    async (err, result) => {
+      if (err) {
+        output({ error: { description: err.message } }, null);
+        console.log(err);
+      } else {
+        const saltRounds = 10;
+        const user = result[3][0].user_id;
+        const verifycode = await bcrypt.hash(otp.toString(), saltRounds);
+        const token = await GENERATE_TOKEN(otp, "1h");
+        const data = { user, verifycode, token };
+        const insertToken = `INSERT INTO tokens (user_id, token, expires_at) VALUES (?, ?, DATE_ADD(NOW(), INTERVAL 5 MINUTE));`;
+        db.query(insertToken, [user.user_id, token], (err, results) => {
+          if (err) {
+            console.error("Database query error:", err);
+            return output({ error: { description: err.message } }, null);
+          }
+          next();
+        });
+        console.log(data);
 
-            const saltRounds = 10;
-            const user = result[3][0].user_id;
-            const verifycode = await bcrypt.hash(otp.toString(), saltRounds);
-            const token = await GENERATE_TOKEN(otp, '1h');
-            const data = { user, verifycode, token };
-            const insertToken = `INSERT INTO tokens (user_id, token, expires_at) VALUES (?, ?, DATE_ADD(NOW(), INTERVAL 5 MINUTE));`; db.query(insertToken, [user.user_id, token], (err, results) => {
-                if (err) {
-                    console.error("Database query error:", err);
-                    return output({ error: { description: err.message } }, null);
-                }
-                next();
-            })
-            console.log(data);
-
-            output(null, data);
-        }
-    });
+        output(null, data);
+      }
+    }
+  );
 };
 
 const otpVerificationServieces = async (input, output) => {
-    const verifiCationOtp = input.verificationotp;
-    const userIdverificationotp = input.userIdverificationotp;
-    const otpverificationquery = `SELECT * FROM otps WHERE user_id = ? AND otp_code = ?`;
-    db.query(otpverificationquery, [userIdverificationotp, verifiCationOtp], (err, result) => {
-        if (err) {
-            output({ error: { description: err.message } }, null);
-            console.log(err);
+  const verifiCationOtp = input.verificationotp;
+  const userIdverificationotp = input.userIdverificationotp;
+  const otpverificationquery = `SELECT * FROM otps WHERE user_id = ? AND otp_code = ?`;
+  db.query(
+    otpverificationquery,
+    [userIdverificationotp, verifiCationOtp],
+    (err, result) => {
+      if (err) {
+        output({ error: { description: err.message } }, null);
+        console.log(err);
+      } else {
+        if (result.length > 0) {
+          output(null, result);
+        } else {
+          output(null, { result: false });
+          console.log(result);
         }
-        else {
-            if (result.length > 0) {
-                output(null, result)
-            }
-            else {
-                output(null, { result: false })
-                console.log(result);
-            }
-
-        }
-    })
-}
+      }
+    }
+  );
+};
 
 const processLoginServieces = async (input, output) => {
-    const userEmail = input.UserMailId;
-    const userPassword = input.UserPassword;
-    const getLoginUserQuery = `SELECT * FROM users WHERE email = ? `
+  const userEmail = input.UserMailId;
+  const userPassword = input.UserPassword;
+  const getLoginUserQuery = `SELECT * FROM users WHERE email = ? `;
 
-    db.query(getLoginUserQuery, [userEmail], async (err, results) => {
-        if (err) {
-            console.error("Database query error:", err);
-            return output({ error: { description: err.message } }, null);
-        }
-        if (results.length === 0) {
-            return output({ error: { message: 'Email not found password.' } }, null);
-        }
-        const user = results[0];
-        const isPasswordValid = await bcrypt.compare(userPassword, user.password);
-        if (!isPasswordValid) {
-            return output({ error: { description: 'Incorrect password.' } }, null);
-        }
-        const { password, ...userWithoutPassword } = user;
-        const token = await GENERATE_TOKEN(user.user_id, '1h');
-        console.log(user.user_id, token);
-        const insertToken = `INSERT INTO tokens (user_id, token, expires_at) VALUES (?, ?, DATE_ADD(NOW(), INTERVAL 5 MINUTE));`; db.query(insertToken, [user.user_id, token], (err, results) => {
-            if (err) {
-                console.error("Database query error:", err);
-                return output({ error: { description: err.message } }, null);
-            }
-            next();
-        })
-        return output(null, { ...userWithoutPassword, token });
+  db.query(getLoginUserQuery, [userEmail], async (err, results) => {
+    if (err) {
+      console.error("Database query error:", err);
+      return output({ error: { description: err.message } }, null);
+    }
+    if (results.length === 0) {
+      return output({ error: { message: "Email not found password." } }, null);
+    }
+    const user = results[0];
+    const isPasswordValid = await bcrypt.compare(userPassword, user.password);
+    if (!isPasswordValid) {
+      return output({ error: { description: "Incorrect password." } }, null);
+    }
+    const { password, ...userWithoutPassword } = user;
+    const token = await GENERATE_TOKEN(user.user_id, "1h");
+    console.log(user.user_id, token);
+    const insertToken = `INSERT INTO tokens (user_id, token, expires_at) VALUES (?, ?, DATE_ADD(NOW(), INTERVAL 5 MINUTE));`;
+    db.query(insertToken, [user.user_id, token], (err, results) => {
+      if (err) {
+        console.error("Database query error:", err);
+        return output({ error: { description: err.message } }, null);
+      }
+      next();
     });
-
-}
+    return output(null, { ...userWithoutPassword, token });
+  });
+};
 const geustSignServices = (input, output) => {
-    const { email, mobilenumber, code: otp } = input;
-    const checkUserQuery = `SELECT * FROM users WHERE email = ? OR phone_number = ?`;
-    db.query(checkUserQuery, [email, mobilenumber], async (err, result) => {
-        if (err) {
-            output({ error: { description: err.message } }, null);
-            return;
-        }
-        if (result.length > 0) {
-            console.log(result);
+  const { email, mobilenumber, code: otp } = input;
+  const checkUserQuery = `SELECT * FROM users WHERE email = ? OR phone_number = ?`;
+  db.query(checkUserQuery, [email, mobilenumber], async (err, result) => {
+    if (err) {
+      output({ error: { description: err.message } }, null);
+      return;
+    }
+    if (result.length > 0) {
+      console.log(result);
 
-            const saltRounds = 10;
-            const userId = result[0].user_id;
-            const token = await bcrypt.hash(otp.toString(), saltRounds);
-            const data = { success: true, userId, description: "User Already Exsist" };
-            output(null, { ...data });
-        }
-        else {
-            const geustSignQuery = `
+      const saltRounds = 10;
+      const userId = result[0].user_id;
+      const token = await bcrypt.hash(otp.toString(), saltRounds);
+      const data = {
+        success: true,
+        userId,
+        description: "User Already Exsist",
+      };
+      output(null, { ...data });
+    } else {
+      const geustSignQuery = `
                         INSERT INTO users (email, phone_number) VALUES (?, ?);
                         SET @last_user_id = LAST_INSERT_ID();
                         SELECT * FROM users WHERE user_id = @last_user_id;`;
-            db.query(geustSignQuery, [email, mobilenumber], async (err, result) => {
-                if (err) {
-                    output({ error: { description: err.message } }, null);
-                    console.log(err);
-                } else {
-                    const saltRounds = 10;
-                    const userId = result[2][0].user_id;
-                    const token = await bcrypt.hash(otp.toString(), saltRounds);
-                    const data = { success: true, userId, description: "User created successfully" };
-                    output(null, data);
-                }
-            });
+      db.query(geustSignQuery, [email, mobilenumber], async (err, result) => {
+        if (err) {
+          output({ error: { description: err.message } }, null);
+          console.log(err);
+        } else {
+          const saltRounds = 10;
+          const userId = result[2][0].user_id;
+          const token = await bcrypt.hash(otp.toString(), saltRounds);
+          const data = {
+            success: true,
+            userId,
+            description: "User created successfully",
+          };
+          output(null, data);
         }
-
-    });
-}
+      });
+    }
+  });
+};
 
 const googleAuthentiCationServices = async (input, output) => {
-    const { Email } = input;
-    const checkUserQuery = `SELECT * FROM users WHERE email = ?`;
-    db.query(checkUserQuery, [Email], async (err, result) => {
-
-        if (err) {
-            output({ error: { description: err.message } }, null);
-        }
-        if (result.length > 0) {
-            console.log(result);
-            // const saltRounds = 10;
-            const userId = result[0].user_id;
-            // const token = await bcrypt.hash(otp.toString(), saltRounds);
-            const data = { success: true, userId, description: "User Already Exsist" };
-            output(null, { ...data });
-        }
-        else {
-            const geustSignQuery = `
+  const { Email } = input;
+  const checkUserQuery = `SELECT * FROM users WHERE email = ?`;
+  db.query(checkUserQuery, [Email], async (err, result) => {
+    if (err) {
+      output({ error: { description: err.message } }, null);
+    }
+    if (result.length > 0) {
+      console.log(result);
+      // const saltRounds = 10;
+      const userId = result[0].user_id;
+      // const token = await bcrypt.hash(otp.toString(), saltRounds);
+      const data = {
+        success: true,
+        userId,
+        description: "User Already Exsist",
+      };
+      output(null, { ...data });
+    } else {
+      const geustSignQuery = `
                         INSERT INTO users (email) VALUES (?);
                         SET @last_user_id = LAST_INSERT_ID();
                         SELECT * FROM users WHERE user_id = @last_user_id;`;
-            db.query(geustSignQuery, [Email], async (err, result) => {
-                console.log(Email);
+      db.query(geustSignQuery, [Email], async (err, result) => {
+        console.log(Email);
 
-                if (err) {
-                    output({ error: { description: err.message } }, null);
-                    console.log(err);
-                } else {
-                    // const saltRounds = 10;
-                    const userId = result[2][0].user_id;
-                    console.log(result);
-                    
-                    // const token = await bcrypt.hash(otp.toString(), saltRounds);
-                    const data = { success: true, userId, description: "User created successfully" };
-                    output(null,data);
-                }
-            });
+        if (err) {
+          output({ error: { description: err.message } }, null);
+          console.log(err);
+        } else {
+          // const saltRounds = 10;
+          const userId = result[2][0].user_id;
+          console.log(result);
+
+          // const token = await bcrypt.hash(otp.toString(), saltRounds);
+          const data = {
+            success: true,
+            userId,
+            description: "User created successfully",
+          };
+          output(null, data);
         }
+      });
+    }
+  });
+};
 
-    });
-}
-module.exports = { authentiCationService, otpVerificationServieces, googleAuthentiCationServices, geustSignServices, processLoginServieces };
+const createUserService = async () => {
+    const name = null;
+    const email = null;
+    const phone_number = null;
+    try {
+        const insertQuery = `INSERT INTO users (name, email, phone_number) VALUES (?, ?, ?)`;
+        const [result] = await db.promise().query(insertQuery, [name, email, phone_number]);
+        if(result.affectedRows > 0) {
+            return result;
+        } else {
+            return {success: false, status: 400, message: "unable to create"}
+        }  
+    } catch (error) {
+        console.error(error);
+        return {success: false, status: 400, message: ""}
+    }
+};
+module.exports = {
+  authentiCationService,
+  otpVerificationServieces,
+  googleAuthentiCationServices,
+  geustSignServices,
+  processLoginServieces,
+  createUserService,
+};
